25371895080777ff4dfb1316da5636acaa452aa4
Mike <madetunji1376@gmail.com>
2022-07-01 18:37:33 +0100

    home-page done

---
+++ Home-page/script.js
@@ -0,0 +1,41 @@
+const data = [
+  {
+    featuredimage: './images/rzfvhmeq6flztj3zddfh.jpg',
+    role: 'Host',
+    currentrole: 'Current Crystal Palalce Manager',
+    name: 'Patrict Viera',
+  },
+
+  {
+    featuredimage: './images/jurgen-klopp-liverpool-25-may-2022-manager.jpg',
+    role: 'Guest Speaker',
+    currentrole: 'Current Liverpool Manager',
+    name: 'Jurgen Klopp',
+  },
+
+  {
+    featuredimage: './images/Arsene-Wenger-02-scaled-e1592114277435.jpg',
+    role: 'Speaker',
+    name: 'Arsene Wenger',
+  },
+
+  {
+    featuredimage: './images/michel-platini_3485945b.jpg',
+    role: 'UEFA President',
+    name: 'Mitchel Platini',
+  },
+];
+
+const projectselection = document.querySelector('.featured-speakers');
+data.forEach((element) => {
+  console.log('element');
+  const allCard = `<div class="featured-image-desktop">
+  <div class="host-desktop">
+     <div><img src="${element.featuredimage}" alt="Viera Picture"></div>
+     <div class="details">${element.role}<br>${element.currentrole}</br><br>${element.name}</br></div>
+  </div>
+</div>
+`;
+
+  projectselection.innerHTML += allCard;
+});
