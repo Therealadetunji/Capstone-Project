26ad0d96827cc4f1aa15aa887c59995679ccf321
Mike <madetunji1376@gmail.com>
2022-07-02 04:40:23 +0100

    rreviewer suggestions applied and corrections implemented

--- Home-page/script.js
+++ Home-page/script.js
@@ -4,6 +4,7 @@ const data = [
     role: 'Host',
     currentrole: 'Current Crystal Palalce Manager',
     name: 'Patrict Viera',
+    descriptiom: 'Former ex Arsenal player and new coach of the eagles',
   },
 
   {
@@ -11,18 +12,22 @@ const data = [
     role: 'Guest Speaker',
     currentrole: 'Current Liverpool Manager',
     name: 'Jurgen Klopp',
+    descriptiom: 'Ex dortmund manager and liverpool boss',
   },
 
   {
     featuredimage: './images/Arsene-Wenger-02-scaled-e1592114277435.jpg',
-    role: 'Speaker',
+    currentrole: 'Speaker',
     name: 'Arsene Wenger',
+    descriptiom: 'Ex arsenal coach but retired now',
   },
 
   {
     featuredimage: './images/michel-platini_3485945b.jpg',
-    role: 'UEFA President',
+    currentrole: 'UEFA President',
     name: 'Mitchel Platini',
+    descriptiom:
+      'Cuurent UEFA President and former ballon Dor Winner three times',
   },
 ];
 
@@ -32,7 +37,11 @@ data.forEach((element) => {
   const allCard = `<div class="image">
   <img src="${element.featuredimage}" alt="Viera Picture">
   <div class="details">
-    <p class="speaker-desc"><br>${element.currentrole}</br>${element.name}</p>
+    <h3>${element.name}</h3>
+    <br>
+    <p class="speaker-desc">${element.currentrole}</p>
+    <br>
+    <p class="for-mobile">${element.descriptiom}</p>
   </div>
 </div>
 `;
